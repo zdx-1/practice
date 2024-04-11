0ef3a60fd4d99f3ef37e4df2992376e29fdc99c2
zdx-1 <19546328912@163.com>
2024-04-11 11:24:38 +0800

    1

--- 模板背诵/并查集存储结构.py
+++ 模板背诵/并查集存储结构.py
@@ -1,18 +1,44 @@
-#第三遍
+# #第三遍
+# Maxn=200
+# fa=[]
+# def init():
+#     for i in range(Maxn+1):
+#         fa.append(i)
+
+# init()
+
+# def find(x):
+#     if fa[x]==x:
+#         return x
+#     else:
+#         return find(fa[x])
+    
+# print(find(20))
+
+# def find(x):
+#     if fa[x]==x:
+#         return x
+#     else:
+#         fa[x]=find(fa[x])
+#         return fa[x]
+
+# print(fa[20])
+
+
+#第四遍
 Maxn=200
 fa=[]
 def init():
     for i in range(Maxn+1):
         fa.append(i)
 
-init()
-
 def find(x):
     if fa[x]==x:
         return x
     else:
         return find(fa[x])
-    
+
+init()
 print(find(20))
 
 def find(x):
@@ -21,5 +47,4 @@ def find(x):
     else:
         fa[x]=find(fa[x])
         return fa[x]
-
-print(fa[20])
+print(find(10))
--- 模板背诵/排列型枚举模板.py
+++ 模板背诵/排列型枚举模板.py
@@ -1,28 +1,216 @@
-"""
-        排列型枚举是n个的全排列,就是从n个中选取n个,然后按顺序输出
-！！！需要注意的是，相比较组合只关心有多少个集合，而排序是关心集合内的排序方式！！！
-"""
-order=[0]*20
+# #第一遍
+# order=[0]*20
+# chosen=[0]*20
+# n=0
+# cnt=0
+
+# def calc(x):
+#     if x==n+1:
+#         ansTemp=""
+#         for i in range(1,n+1):
+#             print(order[i],end=" ")
+#         print()
+#         global cnt
+#         cnt+=1
+#         return
+#     for i in range(1,n+1):
+#         if chosen[i]==1:
+#             continue
+#         order[x]=i
+#         chosen[i]=1
+#         calc(x+1)
+#         chosen[i]=0
+#         order[x]=0
+# n=int(input())
+# calc(1)
+# print(cnt)
+
+# #第二遍
+# order=[0]*20
+# chosen=[0]*20
+# n,cnt=0,0
+# def calc(x):
+#     print("\033[32mn+1=={2},x={3},\norder={0},\nchosen={1}\033[0m".format(order,chosen,n+1,x))
+#     if x==n+1:
+#         ansTemp=""
+#         for i in range(1,n+1):
+#             print(order[i],end=" ")
+#         print()
+#         global cnt
+#         cnt+=1
+#         print("\033[31mn+1=={2},x={3},\norder={0},\nchosen={1}\033[0m".format(order,chosen,n+1,x))
+#         return
+#     for i in range(1,n+1):
+#         if chosen[i]==1:
+#             continue
+#         order[x]=i
+#         chosen[i]=1
+#         calc(x+1)
+#         chosen[i]=0
+#         order[x]=0
+#         print("\033[34morder=\033[0m",order)
+#         print("\033[38mchosen=\033[0m",chosen)
+
+# n=int(input())
+# calc(1)
+# print(cnt)
+
+
+# #第三遍
+# order=[0]*20
+# chosen=[0]*20
+# n=0
+# cnt=0
+# def calc(x):
+#     if x==n+1:
+#         ansTemp=""
+#         for i in range(1,n+1):
+#             print(order[i],end="")
+#         print()
+#         global cnt
+#         cnt+=1
+#         return
+#     for i in range(1,n+1):
+#         if chosen[i]==1:
+#             continue
+#         order[x]=i
+#         chosen[i]=1
+#         calc(x+1)
+#         chosen[i]=0
+#         order[x]=0
+# n=int(input())
+# calc(1)
+# print(cnt)
+
+# #第四遍
+# order,chosen=[0]*20,[0]*20
+# n,cnt=0,0
+# def calc(x):
+#     if x==n+1:
+#         ansTemp=""
+#         for i in range(1,n+1):
+#             print(order[i],end='')
+#         print()
+#         global cnt
+#         cnt+=1
+#         return
+#     for i in range(1,n+1):
+#         if chosen[i]==1:
+#             continue
+#         order[x]=i
+#         chosen[i]=1
+#         calc(x+1)
+#         chosen[i]=0
+#         order[x]=0
+# n=int(input())
+# calc(1)
+# print(cnt)
+
+# #第五遍
+# order=[0]*20
+# chosen=[0]*20
+# n=0
+# cnt=0
+# def calc(x):
+#     if x==n+1:
+#         for i in range(1,n+1):
+#             print(order[i],end="")
+#         print()
+#         global cnt
+#         cnt +=1
+#         return
+#     for i in range(1,n+1):
+#         if chosen[i]==1:
+#             continue
+#         order[x]=i
+#         chosen[i]=1
+#         calc(x+1)
+#         chosen[i]=0
+#         order[x]=0
+# n=int(input())
+# calc(1)
+# print(cnt)
+
+
+# #第六遍
+# order=[0]*20
+# chosen=[0]*20
+# n=0
+# cnt=0
+
+# def calc(x):
+#     if x==n+1:
+#         for i in range(1,n+1):
+#             print(order[i],end="")
+#         print()
+#         global cnt
+#         cnt+=1
+#         return
+#     for i in range(1,n+1):
+#         if chosen[i]==1:
+#             continue
+#         chosen[i]=1
+#         order[x]=i
+#         calc(x+1)
+#         chosen[i]=0
+#         order[x]=0
+# n=int(input())
+# calc(1)
+# print(cnt)
+
+
+#第七遍
+# order=[0]*20
+# chosen=[0]*20
+# n=0 
+# cnt=0
+
+# def calc(x):
+#     if x==n+1:
+#         for i in range(1,n+1):
+#             print(order[i],end="")
+#         print()
+#         global cnt 
+#         cnt+=1
+#         return
+#     for i in range(1,n+1):
+#         if chosen[i]==1:
+#             continue
+#         order[x]=i
+#         chosen[i]=1
+#         calc(x+1)
+#         order[x]=0
+#         chosen[i]=0
+# n=int(input())
+# calc(1)
+# print(cnt)
+
+
+#第八遍
 chosen=[0]*20
+order=[0]*20
 n=0
 cnt=0
+
 def calc(x):
     if x==n+1:
-        ansTemp=''
         for i in range(1,n+1):
-            print(order[i],end=' ')
+            print(order[i],end="")
         print()
-        global cnt
-        cnt+=1
-        return
+        global cnt 
+        cnt +=1
+        return 
+    
     for i in range(1,n+1):
         if chosen[i]==1:
             continue
-        order[x]=i
         chosen[i]=1
+        order[x]=i
         calc(x+1)
         chosen[i]=0
         order[x]=0
+
 n=int(input())
+
 calc(1)
 print(cnt)
