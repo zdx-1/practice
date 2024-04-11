c00980671af6aad6a760e078888f24f1f4ae6e59
zdx-1 <19546328912@163.com>
2024-04-11 09:05:32 +0800

    11

--- 模板背诵/二分查找模板.py
+++ 模板背诵/二分查找模板.py
@@ -47,10 +47,40 @@
 
 
 #第六遍
-a=list(i for i in range(1,11))
-x=4
-low=0
-high=len(a)-1
+# a=list(i for i in range(1,11))
+# x=4
+# low=0
+# high=len(a)-1
+# # while low<high:
+# #     mid=(low+high)//2
+# #     if a[mid]>=x:
+# #         high=mid
+# #     else:
+# #         low=mid+1
+# # print(a[high])
+# while low<high:
+#     mid=(low+high+1)//2
+#     if a[mid]<=x:
+#         low=mid
+#     else:
+#         high=mid-1
+# print(a[high])
+
+
+#第七遍
+# a=list(i for i in range(1,11))
+# x=3
+# low=0
+# high=len(a)-1
+
+# while low<high:
+#     mid=(low+high+1)//2
+#     if a[mid]<=x:
+#         low=mid
+#     else:
+#         high=mid-1
+# print(a[high])
+
 # while low<high:
 #     mid=(low+high)//2
 #     if a[mid]>=x:
@@ -58,10 +88,75 @@ high=len(a)-1
 #     else:
 #         low=mid+1
 # print(a[high])
+
+# #第八遍
+# a=list(i for i in range(1,11))
+# x=4
+# low=0
+# high=len(a)-1
+
+# # while low<high:
+# #     mid=(low+high)//2
+# #     if a[mid]>=x:
+# #         high=mid
+# #     else:
+# #         low=mid+1
+# # print(a[high])
+
+# while low <high:
+#     mid=(low+high+1)//2
+#     if a[mid]<=x:
+#         low=mid
+#     else:
+#         high=mid-1
+# print(a[high])
+
+
+#第九遍
+# a=list(i for i in range(1,11))
+# x=2
+# low=0
+# high=len(a)-1
+
+# while low<high:
+#     mid=(high+low)//2
+#     if a[mid]<=x:
+#         low=mid
+#     else:
+#         high=mid-1
+# print(a[high])
+
+# while low<high:
+#     mid=(high+low+1)//2
+#     if a[mid]>=x:
+#         high=mid
+#     else:
+#         low=mid+1
+
+# print(a[high])
+
+
+#第十遍
+
+a=list(i for i in range(1,11))
+x=3
+low=0
+high=len(a)-1
+
 while low<high:
     mid=(low+high+1)//2
     if a[mid]<=x:
         low=mid
     else:
         high=mid-1
+
+print(a[high])
+
+while low<high:
+    mid=(low+high)//2
+    if a[mid]>=x:
+        high=mid
+    else:
+        low=mid+1
+
 print(a[high])
