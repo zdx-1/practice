4622f168f341b88c403c2517e4e900fc80d18400
zdx-1 <19546328912@163.com>
2024-05-26 10:12:28 +0800

    11

--- S计划/数据结构/队列-机器翻译.py
+++ S计划/数据结构/队列-机器翻译.py
@@ -1,4 +1,20 @@
 """
 https://www.lanqiao.cn/problems/511/learning/
 """
-a=[]
+import os
+import sys
+from collections import deque
+# 请在此输入您的代码
+m,n=map(int,input().split())
+t=[int(i) for i in input().split()]
+q=deque()
+s=0
+for i in t:
+  if i not in q:
+    s=s+1
+    if len(q)<=m-1:
+      q.append(i)
+    else:
+      q.popleft()
+      q.append(i)
+print(s)
